ad35f5653e8eb7253ece4ca9935ba679d46ec3a2
周子鉴 <blqw@live.com>
2017-10-15 01:47:32 +0800

    init

---
+++ solution/blqw.Startup/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System;
+
+#if NET461
+//[assembly: PreApplicationStartMethod(typeof(blqw.Startup), "ApplicationStart")]
+#endif
+
+namespace blqw
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class Startup
+    {
+#if NET461
+
+#endif
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="container"></param>
+        public static void On(IContainer container)
+        {
+
+        }
+
+
+
+    }
+}
