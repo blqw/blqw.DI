611bfd88e6501a30e134da1318e0bfcf3abbb598
blqw <zhouzijian@fastfish.com>
2017-12-27 17:00:40 +0800

    nuget发布

--- nuget-pack.bat
+++ nuget-pack.bat
@@ -3,4 +3,4 @@ dotnet pack -c Release -o %CD%\unpkgs
 for /r . %%a in (unpkgs\*.nupkg) do (
  	nuget push "%%a" -source https://www.nuget.org/api/v2/package -apikey %1%
 )
-del %CD%\unpkgs\*.nupkgnupkg
+del %CD%\unpkgs\*.nupkg
