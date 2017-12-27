6480a0c7bc7d947a5d1385aa24c6bb5fd36c3238
blqw <zhouzijian@fastfish.com>
2017-12-27 16:15:47 +0800

    nuget 调试完成

---
+++ nuget-pack.bat
@@ -0,0 +1,6 @@
+del %CD%\unpkgs\*.nupkg
+dotnet pack -c Release -o %CD%\unpkgs
+for /r . %%a in (unpkgs\*.nupkg) do (
+ 	nuget push "%%a" -source https://www.nuget.org/api/v2/package -apikey %1%
+)
+del %CD%\unpkgs\*.nupkg
