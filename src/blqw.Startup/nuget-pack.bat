51aaa58ef6079aa5a5f2b7aeb2a29bbdad17738a
周子鉴 <blqw@live.com>
2018-06-12 02:01:07 +0800

    优化打包脚本

--- src/blqw.Startup/nuget-pack.bat
+++ src/blqw.Startup/nuget-pack.bat
@@ -1,5 +1,6 @@
 del %CD%\unpkgs\*.nupkg
-dotnet pack -c Release -o %CD%\unpkgs
+dotnet build -c release
+dotnet pack --no-build -o %CD%\unpkgs
 for /r . %%a in (unpkgs\*.nupkg) do (
  	dotnet nuget push "%%a" -s https://api.nuget.org/v3/index.json -k %1%
 )
