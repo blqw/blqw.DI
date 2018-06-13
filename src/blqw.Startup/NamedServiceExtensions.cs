0a49fa68ff3a9544fb87e92376fa7421a190635c
blqw <zhouzijian@fastfish.com>
2018-06-13 20:37:17 +0800

    支持命名服务

---
+++ src/blqw.Startup/NamedServiceExtensions.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace blqw
+{
+    public static class NamedServiceExtensions
+    {
+        public static IServiceCollection AddNamedScoped<TService>(this IServiceCollection services, string name)
+            where TService : class
+        {
+            services.Add(new ServiceDescriptor(new NamedType(name, typeof(TService)), typeof(TService), ServiceLifetime.Scoped));
+            return services;
+        }
+        public static IServiceCollection AddNamedScoped(this IServiceCollection services, string name, Func<IServiceProvider, object> implementationFactory)
+        {
+            services.Add(new ServiceDescriptor(new NamedType(name, null), implementationFactory, ServiceLifetime.Scoped));
+            return services;
+        }
+        public static IServiceCollection AddNamedScoped(this IServiceCollection services, string name, Type serviceType)
+        {
+            services.Add(new ServiceDescriptor(new NamedType(name, serviceType), serviceType, ServiceLifetime.Scoped));
+            return services;
+        }
+
+
+        public static IServiceCollection AddNamedSingleton<TService>(this IServiceCollection services, string name)
+            where TService : class
+        {
+            services.Add(new ServiceDescriptor(new NamedType(name, typeof(TService)), typeof(TService), ServiceLifetime.Singleton));
+            return services;
+        }
+        public static IServiceCollection AddNamedSingleton(this IServiceCollection services, string name, Func<IServiceProvider, object> implementationFactory)
+        {
+            services.Add(new ServiceDescriptor(new NamedType(name, null), implementationFactory, ServiceLifetime.Singleton));
+            return services;
+        }
+        public static IServiceCollection AddNamedSingleton(this IServiceCollection services, string name, Type serviceType)
+        {
+            services.Add(new ServiceDescriptor(new NamedType(name, serviceType), serviceType, ServiceLifetime.Singleton));
+            return services;
+        }
+
+        public static IServiceCollection AddNamedSingleton(this IServiceCollection services, string name, object service)
+        {
+            services.Add(new ServiceDescriptor(new NamedType(name, null), service));
+            return services;
+        }
+
+        public static IServiceCollection AddNamedTransient<TService>(this IServiceCollection services, string name)
+            where TService : class
+        {
+            services.Add(new ServiceDescriptor(new NamedType(name, typeof(TService)), typeof(TService), ServiceLifetime.Transient));
+            return services;
+        }
+        public static IServiceCollection AddNamedTransient(this IServiceCollection services, string name, Func<IServiceProvider, object> implementationFactory)
+        {
+            services.Add(new ServiceDescriptor(new NamedType(name, null), implementationFactory, ServiceLifetime.Transient));
+            return services;
+        }
+        public static IServiceCollection AddNamedTransient(this IServiceCollection services, string name, Type serviceType)
+        {
+            services.Add(new ServiceDescriptor(new NamedType(name, serviceType), serviceType, ServiceLifetime.Transient));
+            return services;
+        }
+    }
+}
---
+++ src/blqw.Startup/NamedType.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace blqw
+{
+    /// <summary>
+    /// 命名类型
+    /// </summary>
+    internal sealed class NamedType : TypeDelegator, IReflectableType
+    {
+        private readonly string _name;
+        private readonly Guid _guid;
+
+        public NamedType(string name, Type delegatingType)
+            : base(delegatingType ?? typeof(object))
+        {
+            _name = name;
+            using (var md5Provider = new MD5CryptoServiceProvider())
+            {
+                var bytes = Encoding.UTF8.GetBytes(name);
+                var hash = md5Provider.ComputeHash(bytes);
+                _guid = new Guid(hash);
+            }
+        }
+        public override Guid GUID => _guid;
+        public override bool Equals(object obj) => Equals(obj as NamedType);
+        public override int GetHashCode() => _name.GetHashCode();
+        public override bool Equals(Type o) => o is NamedType t && t._name == _name;
+        public override string Name => _name;
+        public override string FullName => $"NamedTypes.{_name}";
+        public TypeInfo GetTypeInfo() => BaseType?.GetTypeInfo() ?? new TypeDelegator(this);
+    }
+}
